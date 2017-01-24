3ab72ab092e4017633441a16dd590bfe46481b06
Elise Craig <2191401C@student.gla.ac.uk>
2017-01-24 21:57:49 +0000

    Category view added.

--- rango/models.py
+++ rango/models.py
@@ -1,18 +1,26 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from django.template.defaultfilters import slugify
 
 # Create your models here.
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
-    views = 0;
-    likes = 0;
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique = True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name_plural = 'Categories'
 
     def __str__(self):
+        return self.name
+
+    def __unicode__(self):
         return self.name
 
 class Page(models.Model):
