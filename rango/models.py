65c5eb4b522d9d647113fe528be956a51dbfa96f
Elise Craig <2191401C@student.gla.ac.uk>
2017-01-31 21:22:29 +0000

    Bug fixes.

--- rango/models.py
+++ rango/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -31,3 +32,22 @@ class Page(models.Model):
 
     def __str__(self):
         return self.title
+
+    def __unicode__(self):
+        return self.title
+
+class UserProfile(models.Model):
+    #This line is required. Links UserProfile to a User model instance.
+    user = models.OneToOneField(User)
+
+    #The additional attributes we wish to include.
+    website = models.URLField(blank = True)
+    picture = models.ImageField(upload_to='profile_images', blank = True)
+
+    #Override the __unicode__() method to return something meaningful.
+    #Remember to define __unicode__ too since using python 2.7.x
+    def __str__(self):
+        return self.user.username
+
+    def __unicode__(self):
+        return self.user.username
